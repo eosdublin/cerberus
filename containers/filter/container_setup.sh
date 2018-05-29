1810d9f6c085c6650faea7aa3526413586291d45
Sam Noble <sam@atomconsulting.co>
2018-05-30 00:17:37 +0200

    Removed unused files. Updated default config so things point to the right place.

--- containers/filter/container_setup.sh
+++ containers/filter/container_setup.sh
@@ -3,13 +3,18 @@ TMPDIR=$1
 
 printf ">>> Pushing Patroneos configuration files...\\n"
 # Copy Patroneos files
-rsync -a $TMPDIR/patroneosd /opt/patroneos/
-rsync -a $TMPDIR/patroneos_filter_config.json /etc/patroneos/config.json
-
+sudo mkdir /opt/patroneos/
+sudo cp $TMPDIR/patroneosd /opt/patroneos/
 sudo chown ubuntu:ubuntu /opt/patroneos/patroneosd
+
+sudo mkdir /etc/patroneos
+sudo cp $TMPDIR/patroneos_filter_config.json /etc/patroneos/config.json
+sudo chown ubuntu:ubuntu /etc/patroneos/config.json
+
+# sudo chown ubuntu:ubuntu /opt/patroneos/patroneosd
 sudo chmod u+x /opt/patroneos/patroneosd
 
-echo "Starting patroneosd"
+echo ">>> Starting patroneosd"
 /opt/patroneos/patroneosd -configFile /etc/patroneos/config.json &
 
 #rm -rf $TMPDIR
